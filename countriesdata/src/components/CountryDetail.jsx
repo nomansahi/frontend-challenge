27b9973c2368c4ed690db66b871882a5020aeb84
NOMAN ALI <87797326+nomansahi@users.noreply.github.com>
2023-03-15 19:16:03 +0100

    add languages in country detail component

--- countriesdata/src/components/CountryDetail.jsx
+++ countriesdata/src/components/CountryDetail.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import { Link, useParams } from "react-router-dom";
 
 function CountryDetail() {
@@ -29,77 +29,90 @@ function CountryDetail() {
       <Link to="/" className="btn btn-white shadow m-4">
         &larr; Back
       </Link>
+
       <section className="p-4">
-        {country.map((item) => (
-          <div key={item.population} className="d-flex m-4">
-            <article className="pe-4">
-              <img
-                src={item.flags.svg}
-                alt={item.name.common}
-                style={{ width: "400px" }}
-              />
-            </article>
+        {country.map((item) => {
+          return (
+            <div key={item.population} className="d-flex m-4">
+              <Row>
+                <Col md="6">
+                  <article className="pe-4">
+                    <img
+                      src={item.flags.svg}
+                      alt={item.name.common}
+                      style={{ width: "550px" }}
+                    />
+                  </article>
+                </Col>
+                <Col md="6">
+                  <article className="mt-sm-4">
+                    <h1 className="">{item.name.official}</h1>
+                    <div className="d-flex ">
+                      <ul style={{ listStyle: "none" }}>
+                        <li className="">
+                          Native Name:
+                          <span className="textColor">
+                            {item.name.nativeName[0]}
+                          </span>
+                        </li>
+                        <li>
+                          Capital:
+                          <span className="textColor">{item.capital[0]}</span>
+                        </li>
+                        <li>
+                          Population:
+                          <span className="textColor">
+                            {item.population.toLocaleString()}
+                          </span>
+                        </li>
+                        <li>
+                          Region:{" "}
+                          <span className="textColor">{item.region}</span>
+                        </li>
+                        <li>
+                          Subregion:
+                          <span className="textColor">{item.subregion}</span>
+                        </li>
+                      </ul>
+                      <ul style={{ listStyle: "none" }}>
+                        <li>
+                          Top Level Domain: <span className="textColor"></span>
+                        </li>
+                        <li>
+                          Curriences:
+                          <span className="textColor">{item.curriences}</span>
+                        </li>
+                        <li>
+                          Languages:{" "}
+                          <span className="textColor">
+                            {Object.values(item.languages)}
+                          </span>
+                        </li>
+                      </ul>
+                    </div>
 
-            <article className="ms-4">
-              <h1 className="">{item.name.official}</h1>
-              <div className="d-flex ">
-                <ul style={{ listStyle: "none" }}>
-                  <li className="">
-                    Native Name:
-                    <span className="textColor">{item.name.nativeName[0]}</span>
-                  </li>
-                  <li>
-                    Capital:
-                    <span className="textColor">{item.capital[0]}</span>
-                  </li>
-                  <li>
-                    Population:
-                    <span className="textColor">
-                      {item.population.toLocaleString()}
-                    </span>
-                  </li>
-                  <li>
-                    Region: <span className="textColor">{item.region}</span>
-                  </li>
-                  <li>
-                    Subregion:
-                    <span className="textColor">{item.subregion}</span>
-                  </li>
-                </ul>
-                <ul style={{ listStyle: "none" }}>
-                  <li>
-                    Top Level Domain: <span className="textColor"></span>
-                  </li>
-                  <li>
-                    Curriences:{" "}
-                    <span className="textColor">{item.curriences}</span>
-                  </li>
-                  <li>
-                    Languages:{" "}
-                    <span className="textColor">{item.languages[0]}</span>
-                  </li>
-                </ul>
-              </div>
-
-              {item.borders && (
-                <div className="d-flex">
-                  <h3 className="">Borders Countries:</h3>
-                  <ul className="d-flex ">
-                    {item.borders.map((border, index) => (
-                      <li
-                        key={index}
-                        className="px-3 m-2 fs-6 bg-white shadow px-4"
-                        style={{ listStyle: "none" }}
-                      >
-                        {border}
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-            </article>
-          </div>
-        ))}
+                    {item.borders && (
+                      <div className="d-flex">
+                        <h3 className="">Borders Countries:</h3>
+                        <ul className="d-flex ">
+                          {item.borders.map((border, index) => (
+                            <li
+                              key={index}
+                              className="px-3 m-2 fs-6 bg-white shadow px-4"
+                              style={{ listStyle: "none" }}
+                            >
+                              {border}
+                            </li>
+                          ))}
+                        </ul>
+                      </div>
+                    )}
+                  </article>
+                </Col>
+              </Row>
+            </div>
+          );
+        })}
       </section>
     </Container>
   );
